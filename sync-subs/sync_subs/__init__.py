4b7fc367a0a972e2b2d811bbe0e7847414013f5b
Igor Srdanovic <igorsrdanovic@Igors-MBP.local>
2026-02-24 22:23:28 +0100

    removed sync-subs

--- sync-subs/sync_subs/__init__.py
+++
@@ -1,6 +0,0 @@
-"""sync-subs: automatically detect and fix subtitle timing offset."""
-
-from .core import HAS_FFSUBSYNC, check_sync, fix_sync
-
-__all__ = ["check_sync", "fix_sync", "HAS_FFSUBSYNC"]
-__version__ = "0.1.0"
